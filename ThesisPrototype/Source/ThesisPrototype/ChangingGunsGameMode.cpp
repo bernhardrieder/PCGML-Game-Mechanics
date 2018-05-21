764edb1729f48c01f033acabacc2759dd7eeffb3
Unknown <bernhard.rieder@live.at>
2018-05-20 21:58:29 -0400

    initial code for spawning bots in waves

--- ThesisPrototype/Source/ThesisPrototype/ChangingGunsGameMode.cpp
+++ ThesisPrototype/Source/ThesisPrototype/ChangingGunsGameMode.cpp
@@ -1,7 +1,48 @@
 // Copyright 2018 - Bernhard Rieder - All Rights Reserved.
 
 #include "ChangingGunsGameMode.h"
+#include "TimerManager.h"
 
+AChangingGunsGameMode::AChangingGunsGameMode() : Super()
+{
+	TimeBetweenWaves = 2.f;
+}
 
+void AChangingGunsGameMode::StartPlay()
+{
+	Super::StartPlay();
 
+	prepareForNextWave();
+}
 
+void AChangingGunsGameMode::spawnBotTimerElapsed()
+{
+	spawnNewBot();
+
+	--NumOfBotsToSpawn;
+	if(NumOfBotsToSpawn <= 0)
+	{
+		endWave();
+	}
+}
+
+void AChangingGunsGameMode::startWave()
+{
+	++WaveCount;
+	NumOfBotsToSpawn = 2 * WaveCount;
+	GetWorldTimerManager().SetTimer(timerHandle_BotSpawner, this, &AChangingGunsGameMode::spawnBotTimerElapsed, 1.f, true, 0.f);
+
+}
+
+void AChangingGunsGameMode::endWave()
+{
+	GetWorldTimerManager().ClearTimer(timerHandle_BotSpawner);
+	
+	prepareForNextWave();
+}
+
+void AChangingGunsGameMode::prepareForNextWave()
+{
+	FTimerHandle timerHandle_NextWaveStart;
+	GetWorldTimerManager().SetTimer(timerHandle_NextWaveStart, this, &AChangingGunsGameMode::startWave, TimeBetweenWaves, false);
+}
--- ThesisPrototype/Source/ThesisPrototype/ChangingGunsGameMode.h
+++ ThesisPrototype/Source/ThesisPrototype/ChangingGunsGameMode.h
@@ -14,7 +14,32 @@ class THESISPROTOTYPE_API AChangingGunsGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Game Mode")
+	float TimeBetweenWaves;
 
+public:
+	AChangingGunsGameMode();
+	virtual void StartPlay() override;
 
+protected:
+	//hook for BP to spawn a single bot
+	UFUNCTION(BlueprintImplementableEvent, Category = "Game Mode")
+	void spawnNewBot();
+
+	void spawnBotTimerElapsed();
+
+	//start spawning bots
+	void startWave();
+
+	//Stop spawning bots
+	void endWave();
 	
+	//set timer for next wave start
+	void prepareForNextWave();
+
+protected:
+	FTimerHandle timerHandle_BotSpawner;
+	int32 NumOfBotsToSpawn;
+	int32 WaveCount;
 };
